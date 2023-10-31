3b015a1544763e3036fdac8a3a170f16513c151d
joshua salceda <joshuasalceda0021@gmail.com>
2023-10-31 22:51:10 +0800

    Initial Commit

---
+++ taskbuddy/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'pages/home.html')
+
+def about(request):
+    return render(request, 'pages/about.html')
+
+def login(request):
+    return render(request, 'pages/login.html')
+
+def signup(request):
+    return render(request, 'pages/signup.html')
